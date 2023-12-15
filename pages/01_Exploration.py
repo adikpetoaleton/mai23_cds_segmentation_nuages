ec189072a0f4d46214938c65d9b6c7776bebd27d
Alêton Adikpeto <74425769+aletonadikpeto@users.noreply.github.com>
2023-12-15 12:49:50 +0100

    Update 01_Exploration.py

--- pages/01_Exploration.py
+++ pages/01_Exploration.py
@@ -14,7 +14,7 @@ st.set_page_config(
 
 @st.cache_data
 def load_data():
-    data = pd.read_csv('clouds_large.csv')
+    data = pd.read_csv('clouds_1_xsmall.csv')
     return data
 
 @st.cache_data
