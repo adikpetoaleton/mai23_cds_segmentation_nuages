f04e194d71babdb4f052772a1f5500ac17e81d6b
Alêton Adikpeto <aleton.adikpeto@gmail.com>
2024-02-27 12:39:37 +0100

    Update 27-02-2024_12:40

--- utils.py
+++ utils.py
@@ -177,7 +177,7 @@ def displayMask(imageid, ax, masks, w, h, image_path, hide_axis=False, show_mask
 
     ax.set_title(imageid)
     
-    st.write(img.dtype)
+    st.write(img)
     #ax.imshow(img)
 
     if show_mask:
