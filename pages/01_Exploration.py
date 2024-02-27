b6f16c22ab04471e9b07ec5ddedda1d270262811
Alêton Adikpeto <aleton.adikpeto@gmail.com>
2024-02-27 15:06:26 +0100

    Update 27-02-2024_15:06

--- pages/01_Exploration.py
+++ pages/01_Exploration.py
@@ -252,10 +252,10 @@ if isCharger:
             #     ImageIds = ['002be4f_0', '002be4f_1', '002be4f_3']
             #     showImages(ImageIds, 1, 3, dataframe_2, 2100, 1400, IMG_PATH, hide_axis=True, show_mask=True)
 
-            # exp2 = st.expander("Visualiser des images decrites par des segments compactes")
-            # with exp2:
-            #     ImageIds = ['c22cfa7_3', 'f128b90_1', '6906aa0_3']
-            #     showImages(ImageIds, 1, 3, dataframe_2, 2100, 1400, IMG_PATH, hide_axis=True, show_mask=True)
+            exp2 = st.expander("Visualiser des images decrites par des segments compactes")
+            with exp2:
+                ImageIds = ['c22cfa7_3', 'f128b90_1', '6906aa0_3']
+                showImages(ImageIds, 1, 3, dataframe_2, 2100, 1400, IMG_PATH, hide_axis=True, show_mask=True)
 
             # exp3 = st.expander("Visualiser des images mono-classe decrites par des segments disjoints")
             # with exp3:
