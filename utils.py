536a543367310cffde919eef8a2e1c21250072f7
Alêton Adikpeto <aleton.adikpeto@gmail.com>
2024-02-27 11:19:45 +0100

    Update 27-02-2024_11:19

--- utils.py
+++ utils.py
@@ -184,9 +184,10 @@ def displayMask(imageid, ax, masks, w, h, image_path, hide_axis=False, show_mask
         ax.imshow(all_masks, cmap=cmap, alpha=alpha)
 
 def showImages(ImageIds, grid_x, grid_y, df, img_width, img_height, image_path, hide_axis=True, show_mask=False):
-    fig, axes = plt.subplots(grid_x, grid_y, figsize=(20, 10), layout='constrained')
-    for axe, img_id in zip(axes.flat, ImageIds):
-        displayMask(img_id, axe, df, img_width, img_height, image_path, hide_axis, show_mask)
-    st.pyplot(fig)
+    # fig, axes = plt.subplots(grid_x, grid_y, figsize=(20, 10), layout='constrained')
+    # for axe, img_id in zip(axes.flat, ImageIds):
+    #     displayMask(img_id, axe, df, img_width, img_height, image_path, hide_axis, show_mask)
+    # st.pyplot(fig)
+    st.write("Hello World")
     
 
