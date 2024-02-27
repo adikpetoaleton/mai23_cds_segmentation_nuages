02dbb992c3db3336fe79ba296cd69c1254ee398f
Alêton Adikpeto <aleton.adikpeto@gmail.com>
2024-02-27 11:34:26 +0100

    Update 27-02-2024_11:34

--- utils.py
+++ utils.py
@@ -148,8 +148,7 @@ def displayMask(imageid, ax, masks, w, h, image_path, hide_axis=False, show_mask
 
     img_id = imageid.split('_')[0]
     
-    image_path = image_path
-    img = cv2.imread(image_path + img_id + '.jpg')
+    # img = cv2.imread(image_path + img_id + '.jpg')
 
     # if show_mask:
     #     # Get RLE encoded masks of an image by its imageid and related labels (Flower, Fish...)
@@ -178,17 +177,16 @@ def displayMask(imageid, ax, masks, w, h, image_path, hide_axis=False, show_mask
 
     # ax.set_title(imageid)
     
-    ax.imshow(img)
+    # ax.imshow(img)
 
     # if show_mask:
     #     ax.imshow(all_masks, cmap=cmap, alpha=alpha)
 
 def showImages(ImageIds, grid_x, grid_y, df, img_width, img_height, image_path, hide_axis=True, show_mask=False):
-    # fig, axes = plt.subplots(grid_x, grid_y, figsize=(20, 10), layout='constrained')
-    # for axe, img_id in zip(axes.flat, ImageIds):
-    #     displayMask(img_id, axe, df, img_width, img_height, image_path, hide_axis, show_mask)
-    # st.pyplot(fig)
-    st.write("coucou")
+    fig, axes = plt.subplots(grid_x, grid_y, figsize=(20, 10), layout='constrained')
+    for axe, img_id in zip(axes.flat, ImageIds):
+        displayMask(img_id, axe, df, img_width, img_height, image_path, hide_axis, show_mask)
+    st.pyplot(fig)
 
     
 
