ee4d8abb2d4c627f73a5ab6cf6ac5911e391ffbb
Alêton Adikpeto <aleton.adikpeto@gmail.com>
2024-02-27 19:09:01 +0100

    Update 27-02-2024_19:08

--- pages/01_Exploration.py
+++ pages/01_Exploration.py
@@ -249,7 +249,7 @@ if isCharger:
 
         with st.spinner("Veuillez patienter"):
 
-            exp1 = st.expander("Visualiser des images multi-classes", expanded=False)
+            exp1 = st.expander("Visualiser des images multi-classes", expanded=True)
             with exp1:
                 ImageIds = ['002be4f_0', '002be4f_1', '002be4f_3']
                 showImages(ImageIds, 1, 3, dataframe_2, 2100, 1400, IMG_PATH, hide_axis=True, show_mask=True)
