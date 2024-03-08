b5618bd95c0210d5433a113ea22f1296a8ba07c1
Alêton Adikpeto <aleton.adikpeto@gmail.com>
2024-03-08 15:41:32 +0100

    Update 08-03-2024 15:41

--- pages/03_Modélisation (Yolo simplified).py
+++ pages/03_Modélisation (Yolo simplified).py
@@ -282,8 +282,8 @@ if isCharger:
             model = tf.keras.models.load_model(MODEL_FILE, custom_objects={'global_loss': global_loss, 'mean_iou': mean_iou})
 
     if st.button('Prédire'):
-        fName = 'ffd3dfb.jpg'
-        imLabel = 'ffd3dfb.jpg_Gravel'
+        fName = '0011165.jpg'
+        imLabel = '0011165.jpg_Fish'
         img = tf.io.read_file(RESIZED_PATH + fName)
         img = tf.image.decode_png(img, channels=3)
 
