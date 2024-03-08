8886edeb821a2ce971c19424fbd9db49985bc342
Alêton Adikpeto <aleton.adikpeto@gmail.com>
2024-03-08 15:56:58 +0100

    Update 08-03-2024 15:56

--- pages/03_Modélisation (Yolo simplified).py
+++ pages/03_Modélisation (Yolo simplified).py
@@ -289,7 +289,7 @@ if isCharger:
 
         fi4, axes = plt.subplots(1, 2, figsize=(12, 7))
         show_prediction(img , model, axes[0], threshold=0.2)
-        show_ground(imLabel, axes[1], dataframe_2, ORIGINAL_IMAGE_WIDTH, ORIGINAL_IMAGE_HEIGHT, IMAGES_PATH, hide_axis=False, show_mask=True)
+        #show_ground(imLabel, axes[1], dataframe_2, ORIGINAL_IMAGE_WIDTH, ORIGINAL_IMAGE_HEIGHT, IMAGES_PATH, hide_axis=False, show_mask=True)
                     
         plt.tight_layout()
         st.pyplot(fig4)
