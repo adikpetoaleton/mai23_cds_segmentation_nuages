7955e4edf147fada6bcf8d6a8eed085373a7059e
hymnuade <arnaud.kreweras@gmail.com>
2024-03-11 18:23:06 +0100

    slidebar for all

--- Home.py
+++ Home.py
@@ -14,8 +14,15 @@ image = 'utils\cloudiatlas.png'
 
 # Ajoutez l'image à la barre latérale
 st.sidebar.image(image, caption='Référence littéraire et cinématographique', use_column_width=True)
-
-
+st.sidebar.divider()
+st.sidebar.text("Avec la participation de :")
+st.sidebar.markdown("***Alêton ADIKPETO***")
+st.sidebar.markdown("***Soudjad CASSAM-CHENAI***")
+st.sidebar.markdown("***Arnaud KREWERAS***")
+st.sidebar.divider()
+st.sidebar.text("Et comme guest stars :")
+st.sidebar.markdown("***Aziz***")
+st.sidebar.markdown("***Alban*** ")
 
 st.title(":one: Segmentation de régions nuageuses")
 st.write("<br><p style='text-align: justify;'>Depuis de nombreuses années, le changement climatique est au cœur des préoccupations\
--- pages/03_Modélisation (Yolo simplified).py
+++ pages/03_Modélisation (Yolo simplified).py
@@ -24,6 +24,21 @@ import matplotlib.gridspec as gridspec
 from utility_for_modeling import *
 import io
 
+st.sidebar.title('CloudIAtlas Project')
+# Chargez l'image
+image = 'utils\cloudiatlas.png'
+
+# Ajoutez l'image à la barre latérale
+st.sidebar.image(image, caption='Référence littéraire et cinématographique', use_column_width=True)
+st.sidebar.divider()
+st.sidebar.text("Avec la participation de :")
+st.sidebar.markdown("***Alêton ADIKPETO***")
+st.sidebar.markdown("***Soudjad CASSAM-CHENAI***")
+st.sidebar.markdown("***Arnaud KREWERAS***")
+st.sidebar.divider()
+st.sidebar.text("Et comme guest stars :")
+st.sidebar.markdown("***Aziz***")
+st.sidebar.markdown("***Alban*** ")
 
 tf.keras.backend.clear_session()
 
