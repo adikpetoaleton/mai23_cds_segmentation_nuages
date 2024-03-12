d037963c8b30335528e9b3fe7596bbe6994ea023
Alêton Adikpeto <aleton.adikpeto@gmail.com>
2024-03-12 02:46:31 +0100

    Update 02_Modélisation (Yolo simplified).py

--- pages/02_Modélisation (Yolo simplified).py
+++ pages/02_Modélisation (Yolo simplified).py
@@ -87,6 +87,10 @@ def load_hist_data():
     data = pd.read_csv(HIST_FILE)
     return data
 
+@st.cache_data
+def load_model():
+    return tf.keras.models.load_model(MODEL_FILE, custom_objects={'global_loss': global_loss, 'mean_iou': mean_iou})
+
 ###########################################
 # Initialisation des variables de session #
 ###########################################
@@ -299,16 +303,16 @@ if isCharger3:
 
     # Chargement du modèle depuis la sauvegrade model.h5
     if os.path.exists(MODEL_FILE):
-        model = tf.keras.models.load_model(MODEL_FILE, custom_objects={'global_loss': global_loss, 'mean_iou': mean_iou})
+        model = load_model()
 
     if st.button('Prédire'):
-        fName = '2cf739a.jpg'
-        imLabel = '2cf739a.jpg_Fish'
+        fName = '002be4f.jpg'
+        imLabel = '002be4f.jpg_Flower'
         img = tf.io.read_file(RESIZED_PATH + fName)
         img = tf.image.decode_png(img, channels=3)
 
         fig4, axes = plt.subplots(1, 2, figsize=(12, 7))
-        result = show_prediction(img , model, axes[0], threshold=0.6)
+        show_prediction(img , model, axes[0], threshold=0.6)
         show_ground(imLabel, axes[1], dataframe_21, ORIGINAL_IMAGE_WIDTH, ORIGINAL_IMAGE_HEIGHT, IMAGES_PATH, hide_axis=False, show_mask=True)
                     
         plt.tight_layout()
